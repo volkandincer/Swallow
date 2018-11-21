fe7ce10c2c1810f39788c4ccd9f9c77f0cd89dc5
Ahmet <ahmetuztasi@gmail.com>
2018-11-21 22:19:52 +0300

    son konumu alıp ekrana basabiliyoruz, fakat o anki konumunu alamıyorum

--- app/src/main/java/com/example/ahmet/swallow/MainActivity.java
+++ app/src/main/java/com/example/ahmet/swallow/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.ahmet.swallow;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     };
+    Button buton1;
+    TextView enlem, boylam;
+    private FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +54,43 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //*****************
+        buton1 = (Button) findViewById(R.id.button);
+
+        enlem = (TextView) findViewById(R.id.textView4);
+        enlem.setText("ahmet");
+
+        boylam = (TextView) findViewById(R.id.textView5);
+        boylam.setText("tuztasi");
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+        requestPermission();
+
+        buton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                client.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if(location != null){
+                            enlem.setText(location.toString());
+                            boylam.setText("location isn't null");
+                        }
+                    }
+                });
+            }
+        });
+
+
+
+        //******************
+    }
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, 1);
     }
 
 }
