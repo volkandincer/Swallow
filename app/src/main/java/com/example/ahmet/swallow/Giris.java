d5e35a42d231ac0720e8ada21e3a210d3efff7ab
Ahmet <ahmetuztasi@gmail.com>
2019-03-07 23:35:45 +0300

    giris layout ve giris classı eklendı

---
+++ app/src/main/java/com/example/ahmet/swallow/Giris.java
@@ -0,0 +1,14 @@
+package com.example.ahmet.swallow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Giris extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.giris);
+
+    }
+}
